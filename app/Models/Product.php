e66b744eba6748d9f9e3b063000d32b3a5e15c62
EasinTanvir <easintanvir102@gmail.com>
2026-05-14 00:23:53 +0600

    added payment

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,5 +18,12 @@ class Product extends Model
         return $this->belongsToMany(Category::class);
     }
 
+    public function orders()
+{
+    return $this->belongsToMany(Order::class)
+        ->withPivot('quantity', 'price')
+        ->withTimestamps();
+}
+
 
     }

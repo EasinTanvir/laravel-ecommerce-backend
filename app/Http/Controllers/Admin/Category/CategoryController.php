6329816a2fe1ef511207c95bc0361763746da614
EasinTanvir <easintanvir102@gmail.com>
2026-05-13 23:38:34 +0600

    refactor

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -25,7 +25,7 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(CategoryRequest  $request)
+    public function store(CategoryRequest $request)
 {
     $category = Category::create([
         'name' => $request->name,

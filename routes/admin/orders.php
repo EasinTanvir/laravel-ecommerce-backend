22b6c3ddf40ef3030c75ba5fe56a267cc0acb4d6
EasinTanvir <easintanvir102@gmail.com>
2026-05-23 01:18:03 +0600

    update order

--- routes/admin/orders.php
+++ routes/admin/orders.php
@@ -3,9 +3,21 @@
 use App\Http\Controllers\Admin\Orders\OrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('orders', OrderController::class)
-    ->except(['store', 'update', 'destroy']);
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::apiResource('orders', OrderController::class)
-    ->only(['store', 'update', 'destroy'])
-    ->middleware(['auth:sanctum', 'admin']);
+    Route::post('/orders', [OrderController::class, 'store']);
+
+    Route::get('/orders/{order}', [OrderController::class, 'show']);
+
+});
+
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+
+    Route::get('/orders', [OrderController::class, 'index']);
+
+    Route::patch('/orders/{order}', [OrderController::class, 'update']);
+
+    Route::delete('/orders/{order}', [OrderController::class, 'destroy']);
+
+});
